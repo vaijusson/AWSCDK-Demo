269c6b58d226bb0d581d59aa81fa9dea527284b8
Apar V <82201916+vaijusson@users.noreply.github.com>
2021-11-30 21:12:36 +0530

    Initial commit

---
+++ test/awscdk.test.js
@@ -0,0 +1,18 @@
+// const { Template } = require('@aws-cdk/assertions');
+// const cdk = require('@aws-cdk/core');
+// const Awscdk = require('../lib/awscdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/awscdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Awscdk.AwscdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
+
